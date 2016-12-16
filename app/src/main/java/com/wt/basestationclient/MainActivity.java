7fad26fdb51f365a185f2bf3568c7c9c3a3b2a88
teng.wang <teng.wang@corp.elong.com>
2016-12-16 22:21:06 +0800

    change main

--- app/src/main/java/com/wt/basestationclient/MainActivity.java
+++ app/src/main/java/com/wt/basestationclient/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wt.basestationclient;
 
 import android.content.Intent;
+import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +26,32 @@ public class MainActivity extends AppCompatActivity {
         Button queryBtn = (Button) findViewById(R.id.query);
         Button modifyBtn = (Button) findViewById(R.id.modify);
 
+        final EditText editText = (EditText) findViewById(R.id.main_station_edit);
+
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                Intent intent = new Intent(MainActivity.this,RecordScrollingActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
         recordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,RecordScrollingActivity.class);
-                startActivity(intent);
+
+                String stationName = editText.getText().toString().trim();
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        AndroidHttpClient 
+
+                    }
+                });
+
 
             }
         });
